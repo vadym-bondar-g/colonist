714c83db3ee9f112b44272c512ff2335144765fb
vadym-bondar-g <evansfalter@gmail.com>
2025-08-18 18:20:03 +0200

    main fixed all problems, missed code with EBO, fixed raw literal string. Now everything is working

--- src/build/CMakeFiles/Makefile.cmake
+++ src/build/CMakeFiles/Makefile.cmake
@@ -7,7 +7,7 @@ set(CMAKE_DEPENDS_GENERATOR "Unix Makefiles")
 # The top level Makefile was generated from the following files:
 set(CMAKE_MAKEFILE_DEPENDS
   "CMakeCache.txt"
-  "/home/user/colonist/CMakeLists.txt"
+  "/home/user/colonist/src/CMakeLists.txt"
   "CMakeFiles/3.28.3/CMakeCCompiler.cmake"
   "CMakeFiles/3.28.3/CMakeCXXCompiler.cmake"
   "CMakeFiles/3.28.3/CMakeSystem.cmake"
@@ -38,6 +38,8 @@ set(CMAKE_MAKEFILE_DEPENDS
   "/usr/share/cmake-3.28/Modules/Platform/Linux.cmake"
   "/usr/share/cmake-3.28/Modules/Platform/UnixPaths.cmake"
   "/usr/share/cmake-3.28/Modules/SelectLibraryConfigurations.cmake"
+  "/usr/share/cmake/glm/glmConfig.cmake"
+  "/usr/share/cmake/glm/glmConfigVersion.cmake"
   )
 
 # The corresponding makefile is:
--- src/build/CMakeFiles/colonist.dir/DependInfo.cmake
+++ src/build/CMakeFiles/colonist.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/user/colonist/main.cpp" "CMakeFiles/colonist.dir/main.cpp.o" "gcc" "CMakeFiles/colonist.dir/main.cpp.o.d"
+  "/home/user/colonist/src/main.cpp" "CMakeFiles/colonist.dir/main.cpp.o" "gcc" "CMakeFiles/colonist.dir/main.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
