0b816c6f59bd5dffade62c3f807c91f68d509962
vadym-bondar-g <evansfalter@gmail.com>
2025-08-22 20:11:38 +0200

    new classes but without realiztion

--- src/World/WorldManager.h
+++ src/World/WorldManager.h
@@ -1,1 +1,22 @@
+#pragma once
 
+
+#include <string>
+#include <memory>
+#include <vector>
+
+class Entity;
+
+
+
+class WorldManager
+{
+public:
+	bool LoadLevel(const std::string& levelJsonPath);
+	void Update(float dt);
+	void Draw();
+	void Clear();
+	const std::vector<std::unique_ptr<Entity>>& GetEntities() const{ return m_Entities;} ;
+private:
+	std::vector<std::unique_ptr<Entity>> m_Entities;	
+};
