8fe5d8c8dae3b83fcd7cd5e9f563aa3e13ae8445
vadym-bondar-g <evansfalter@gmail.com>
2025-08-24 18:49:02 +0200

    preparing worldManager

--- src/World/WorldManager.h
+++ src/World/WorldManager.h
@@ -1,6 +1,7 @@
 #pragma once
 
-
+#include <glm/glm.hpp>
+#include <glad/glad.h>
 #include <string>
 #include <memory>
 #include <vector>
@@ -12,11 +13,24 @@ class Entity;
 class WorldManager
 {
 public:
+	bool Init(const std::string& objPath);// 24.08
 	bool LoadLevel(const std::string& levelJsonPath);
 	void Update(float dt);
+	void Render(const glm::mat4& view, const glm::mat4& proj);//24.08
+	void Shutdown();// 24.08
 	void Draw();
 	void Clear();
 	const std::vector<std::unique_ptr<Entity>>& GetEntities() const{ return m_Entities;} ;
 private:
 	std::vector<std::unique_ptr<Entity>> m_Entities;	
+
+
+	// GL resources
+	GLuint m_Vao = 0 , m_VBO = 0 , m_EBO = 0;
+	GLuint m_Program = 0;
+	GLsizei m_IndexCount = 0;
+	glm::mat4 m_Model{1.0f};
+
+	bool loadOBJ(const std::string& path, std::vector<float>& interleaved, std::vector<uint32_t>& indices);
+	GLuint makeProgram(const char* vsSrc, const char* fsSrc);
 };
